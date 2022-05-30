73b118927e6772292368dbf8e1074758d6a4d586
Lucas Baltar Esnaty Boudoux <lucasesnaty@gmail.com>
2022-05-30 19:06:37 -0300

    Implementando o componente Entrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    CadastrarComponent
+    RodapeComponent,
+    EntrarComponent,
   ],
   imports: [
     BrowserModule,
