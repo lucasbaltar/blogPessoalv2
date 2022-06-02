b62a0d9e7e46a8bc90e8e5ebe611ad56797ca51b
Lucas Baltar Esnaty Boudoux <lucasesnaty@gmail.com>
2022-06-02 14:32:18 -0300

    Início de Model

---
+++ src/app/model/postagem.ts
@@ -0,0 +1,13 @@
+import { tema } from "./tema"
+import { user } from "./user"
+
+export class postagem{
+    public id: number
+    public titulo: string
+    public texto: string
+    public date: Date
+    public foto: string
+    public usuario: user
+    public tema: tema
+
+}
